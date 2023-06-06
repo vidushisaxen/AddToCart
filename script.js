bd17ffe3700b42bdb0d086c3848a14b8660ff33d
vidushisaxen <vidushisaxenabdn786@gmail.com>
2023-06-06 20:05:15 +0530

    main

---
+++ script.js
@@ -0,0 +1,6 @@
+const inputEl = document.getElementById("input-field")
+const addEl = document.getElementById("add-button")
+addEl.addEventListener("click",()=>{
+    let inputValue = inputEl.value;
+    console.log(inputValue)
+})
