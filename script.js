c2d4c8e913aeb2cd94649f8a9d1d0bf4b91deea2
vidushisaxen <vidushisaxenabdn786@gmail.com>
2023-06-07 21:43:35 +0530

    main

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {getDatabase,ref,push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseUrl :"https://cartisitic-default-rtdb.firebaseio.com/"
+}
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const moviesInDB = ref(database,"movies")
+
+
 const inputEl = document.getElementById("input-field")
 const addEl = document.getElementById("add-button")
 addEl.addEventListener("click",()=>{
     let inputValue = inputEl.value;
-    console.log(inputValue)
+    push(moviesInDB,inputValue)
+    console.log(`${inputValue} added to database`)
 })
